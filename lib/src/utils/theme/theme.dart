3658902adfeda8ed3e3e201fdfd5bbfdd25d8db5
Kidus-github <kidus.muluwork@gmail.com>
2024-12-13 19:37:15 +0300

    editied the theme structure and added dark and light themes

--- lib/src/utils/theme/theme.dart
+++ lib/src/utils/theme/theme.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
-  static ThemeData lightTheme = ThemeData(brightness: Brightness.light);
+  AppTheme._();
+  static ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    textTheme: TextTheme(
+      headlineLarge: GoogleFonts.montserrat(
+        color: Colors.black87,
+      ),
+    ),
+  );
   static ThemeData darkTheme = ThemeData(brightness: Brightness.dark);
 }
