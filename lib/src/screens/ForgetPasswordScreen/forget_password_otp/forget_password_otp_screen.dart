8fd0d0630cf1d0a4082e6c042fc87b0307226ac7
Kidus-github <kidus.muluwork@gmail.com>
2024-12-24 20:41:35 +0300

    otp screeen finalized and completed

--- lib/src/screens/ForgetPasswordScreen/forget_password_otp/forget_password_otp_screen.dart
+++ lib/src/screens/ForgetPasswordScreen/forget_password_otp/forget_password_otp_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:id/src/constants/text_string.dart';
 
@@ -34,6 +35,36 @@ class ForgetPasswordOtpScreen extends StatelessWidget {
                 kOtpMessage + "support@gmail.com",
                 textAlign: TextAlign.center,
               ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              OtpTextField(
+                numberOfFields: 6,
+                fillColor: Colors.black.withOpacity(0.1),
+                filled: true,
+                keyboardType: TextInputType.number,
+                onSubmit: (value) => print('OTP is => $value'),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: Colors.black, // Text and icon color
+                          textStyle: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                          padding: const EdgeInsets.all(20.0),
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.circular(10.0)) // Text style
+                          ),
+                      onPressed: () {},
+                      child: const Text(kNext)))
             ],
           ),
         ),
