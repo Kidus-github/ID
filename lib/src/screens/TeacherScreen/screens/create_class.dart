e6a68e9d7b47a55847fed50d53e17739f5b4fb4e
Kidus-github <kidus.muluwork@gmail.com>
2025-01-01 23:13:41 +0300

    worked on the create screen with all the text fields

--- lib/src/screens/TeacherScreen/screens/create_class.dart
+++ lib/src/screens/TeacherScreen/screens/create_class.dart
@@ -27,35 +27,224 @@ class _CreateClassState extends State<CreateClass> {
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32.0),
               ),
               const SizedBox(
-                height: 10.0,
+                height: 3.0,
               ),
               const Divider(
                 thickness: 2,
                 color: Color(0xffD9D9D9),
               ),
               const SizedBox(
-                height: 10.0,
+                height: 3.0,
               ),
-              Container(
-                margin: const EdgeInsets.symmetric(vertical: 10.0),
-                padding: const EdgeInsets.only(top: 5.0),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    border: Border.all(
-                        color: Colors.black,
-                        width: 1.0,
-                        style: BorderStyle.solid,
-                        strokeAlign: BorderSide.strokeAlignInside)),
-                child: SfCalendar(
-                  view: CalendarView.month,
-                  cellBorderColor: Colors.black,
-                  todayHighlightColor: Colors.black,
-                  // initialDisplayDate: DateTime(2020, 11, 10, 08, 30),
-                  dataSource: ClassDataSource(getAppointments()),
+              const TextField(
+                decoration: InputDecoration(
+                  hintText: kNameHint,
+                  border: UnderlineInputBorder(),
                 ),
               ),
-              Row(
-                children: [TextField(), TextField()],
+              const SizedBox(
+                height: 8.0,
+              ),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 10.0),
+                        padding: const EdgeInsets.only(top: 5.0),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            border: Border.all(
+                                color: Colors.black,
+                                width: 1.0,
+                                style: BorderStyle.solid,
+                                strokeAlign: BorderSide.strokeAlignInside)),
+                        child: SfCalendar(
+                          view: CalendarView.month,
+                          cellBorderColor: Colors.black,
+                          todayHighlightColor: Colors.black,
+
+                          // initialDisplayDate: DateTime(2020, 11, 10, 08, 30),
+                          dataSource: ClassDataSource(getAppointments()),
+                        ),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Checkbox(value: true, onChanged: null),
+                            Text(kOneDayEvent)
+                          ],
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                kFrom,
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              GestureDetector(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(3.0),
+                                      border: Border.all(
+                                          color: Colors.black,
+                                          width: 0.3,
+                                          style: BorderStyle.solid,
+                                          strokeAlign:
+                                              BorderSide.strokeAlignInside)),
+                                  padding: const EdgeInsets.all(6.0),
+                                  child: const Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Text(kEndDateSelected),
+                                          SizedBox(
+                                            width: 10,
+                                          ),
+                                          Text(kEndTimeSelected),
+                                        ],
+                                      ),
+                                      SizedBox(
+                                        width: 8.0,
+                                      ),
+                                      Icon(Icons.edit_calendar_outlined)
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(kTo,
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              GestureDetector(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(3.0),
+                                      border: Border.all(
+                                          color: Colors.black,
+                                          width: 0.5,
+                                          style: BorderStyle.solid,
+                                          strokeAlign:
+                                              BorderSide.strokeAlignInside)),
+                                  padding: const EdgeInsets.all(6.0),
+                                  child: const Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Text(kEndDateSelected),
+                                          SizedBox(
+                                            width: 10,
+                                          ),
+                                          Text(kEndTimeSelected),
+                                        ],
+                                      ),
+                                      SizedBox(
+                                        width: 8.0,
+                                      ),
+                                      Icon(Icons.edit_calendar_outlined)
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 15.0,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                            labelText: kRepeat,
+                            hintText: kOneDayEvent,
+                            labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                            border: UnderlineInputBorder(),
+                            focusedBorder: UnderlineInputBorder()),
+                      ),
+                      const SizedBox(
+                        height: 15.0,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                            hintText: kDescription,
+                            hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                            border: UnderlineInputBorder(),
+                            focusedBorder: UnderlineInputBorder()),
+                      ),
+                      const SizedBox(
+                        height: 15.0,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                            labelText: kAddCoTeacher,
+                            labelStyle: TextStyle(fontWeight: FontWeight.bold),
+                            hintText: "None",
+                            border: UnderlineInputBorder(),
+                            focusedBorder: UnderlineInputBorder()),
+                      ),
+                      const SizedBox(
+                        height: 15.0,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                            hintText: kLocation,
+                            hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                            suffixIcon: Icon(
+                              Icons.add_location,
+                              color: Color(0xff737373),
+                              size: 26,
+                            ),
+                            border: UnderlineInputBorder(),
+                            focusedBorder: UnderlineInputBorder()),
+                      ),
+                      const SizedBox(
+                        height: 25.0,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            side: const BorderSide(color: Colors.black),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 8.0, horizontal: 44.0)),
+                        child: const Text(
+                          kPageTitle,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               )
             ],
           ),
