516978243e2586dd682deb0bec2278996e30ef9a
Kidus-github <kidus.muluwork@gmail.com>
2025-01-13 22:45:22 +0300

    created a header dart for the sign in screen

---
+++ lib/src/screens/SignInScreen/widget/header.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:id/src/constants/image_string.dart';
+import 'package:id/src/constants/text_string.dart';
+import 'package:id/src/screens/SignUpScreen/sign_up_screen.dart';
+
+class HeaderWidget extends StatelessWidget {
+  const HeaderWidget({
+    super.key,
+    required this.size,
+  });
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Image(
+          image: const AssetImage(kLogo),
+          width: size.width * 0.1,
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 50),
+          child: const Text(
+            kSignInText1,
+            style: TextStyle(
+              fontSize: 38.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        RichText(
+          text: TextSpan(children: [
+            const TextSpan(text: kSignInText2, style: TextStyle(fontSize: 14)),
+            TextSpan(
+              text: kSignInText3,
+              style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  decoration: TextDecoration.underline,
+                  color: Colors.blue),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SignUpScreen()));
+                },
+            )
+          ], style: const TextStyle(color: Colors.black)),
+        ),
+      ],
+    );
+  }
+}
