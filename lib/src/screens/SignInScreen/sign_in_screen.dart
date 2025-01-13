7361bd1cadd0aa93602f2382ec1c85352573ec19
Kidus-github <kidus.muluwork@gmail.com>
2025-01-13 22:46:37 +0300

    integrated other widget to the sign in screen

--- lib/src/screens/SignInScreen/sign_in_screen.dart
+++ lib/src/screens/SignInScreen/sign_in_screen.dart
@@ -1,11 +1,8 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
-import 'package:id/src/constants/colors.dart';
-import 'package:id/src/constants/image_string.dart';
 import 'package:id/src/constants/text_string.dart';
-import 'package:id/src/screens/ForgetPasswordScreen/forget_password_screen/forget_password_model_bottom_sheet.dart';
-import 'package:id/src/screens/SignUpScreen/sign_up_screen.dart';
+import 'package:id/src/screens/SignInScreen/widget/header.dart';
+import 'package:id/src/screens/SignInScreen/widget/sign_in_form_widget.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -27,102 +24,8 @@ class _SignInScreenState extends State<SignInScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Image(
-                  image: const AssetImage(kLogo),
-                  width: size.width * 0.1,
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 50),
-                  child: const Text(
-                    kSignInText1,
-                    style: TextStyle(
-                      fontSize: 38.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                RichText(
-                  text: TextSpan(children: [
-                    const TextSpan(
-                        text: kSignInText2, style: TextStyle(fontSize: 14)),
-                    TextSpan(
-                      text: kSignInText3,
-                      style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                          decoration: TextDecoration.underline,
-                          color: Colors.blue),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const SignUpScreen()));
-                        },
-                    )
-                  ], style: const TextStyle(color: Colors.black)),
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(vertical: 50.0),
-                  child: Form(
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.person_outline_outlined),
-                            labelText: kSignInText4,
-                            hintText: kSignInText4,
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 20.0,
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                              prefixIcon: Icon(Icons.fingerprint),
-                              labelText: kSignInText5,
-                              hintText: kSignInText5,
-                              border: OutlineInputBorder(),
-                              suffixIcon: IconButton(
-                                  onPressed: null,
-                                  icon: Icon(Icons.remove_red_eye))),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    ForgotPasswordScreen.buildShowModelBottomSheet(context);
-                  },
-                  child: const Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      kSignInText6,
-                      style: TextStyle(decoration: TextDecoration.underline),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: size.height * 0.03,
-                ),
-                Center(
-                  child: Container(
-                    width: size.width * 0.7,
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 15, horizontal: 111.0),
-                    decoration: BoxDecoration(
-                      color: kDarkColor,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: const Align(
-                      alignment: Alignment.center,
-                      child: Text(kSignInText7,
-                          style: TextStyle(color: Colors.white, fontSize: 16)),
-                    ),
-                  ),
-                ),
+                HeaderWidget(size: size),
+                SignInFormWidget(size: size),
                 const Padding(
                   padding: EdgeInsets.symmetric(vertical: 40.0),
                   child: Row(
@@ -147,27 +50,28 @@ class _SignInScreenState extends State<SignInScreen> {
                 GestureDetector(
                   onTap: () {},
                   child: Container(
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        border: Border.all(color: Colors.black, width: 1),
-                      ),
-                      child: const Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Bootstrap.google,
-                            size: 21,
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Text(
-                            kSignInText9,
-                            style: TextStyle(fontSize: 16),
-                          )
-                        ],
-                      )),
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(color: Colors.black, width: 1),
+                    ),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Bootstrap.google,
+                          size: 21,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          kSignInText9,
+                          style: TextStyle(fontSize: 16),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
                 const SizedBox(
                   height: 20,
