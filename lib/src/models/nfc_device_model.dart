c9a7ca0bd9288d93a34a5921a8b753e97f9a1572
Kidus-github <kidus.muluwork@gmail.com>
2025-01-25 18:40:51 +0300

    updated the nfc module to include json conversion for firestore and added additonal property

--- lib/src/models/nfc_device_model.dart
+++ lib/src/models/nfc_device_model.dart
@@ -1,24 +1,25 @@
 class NfcDeviceModule {
   final String? deviceId;
+  final String ownerId;
   final String deviceName;
   final String deviceLocation;
   final String assignedTo;
   final DateTime createdAt;
 
   NfcDeviceModule(
-      {required this.deviceName,
+      {required this.ownerId,
+      required this.deviceName,
       required this.assignedTo,
       required this.createdAt,
       required this.deviceLocation,
       this.deviceId});
 
-  toJson() {
-    return {
-      "DeviceID": deviceId,
-      "DeviceName": deviceName,
-      "DeviceLocation": deviceLocation,
-      "CreatedAt": createdAt,
-      "AssignedTo": assignedTo,
-    };
-  }
+  toJson() => {
+        "DeviceID": deviceId,
+        "OwnerID": ownerId,
+        "DeviceName": deviceName,
+        "DeviceLocation": deviceLocation,
+        "CreatedAt": createdAt,
+        "AssignedTo": assignedTo,
+      };
 }
