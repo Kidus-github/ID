235ae05df90245ce5cbac47b697766709a46d6d9
Kidus-github <kidus.muluwork@gmail.com>
2025-01-31 22:05:15 +0300

    modified the class model

--- lib/src/models/class_model.dart
+++ lib/src/models/class_model.dart
@@ -1,32 +1,26 @@
 import 'package:id/src/models/repetetion_rule_model.dart';
 
 class ClassModel {
-  final String? classId;
   final String className;
   final String teacherId;
-  final Map<String, String>? coTeacherId; // Corrected to Map<String, String>
+  final List<String>? coTeacherId; // Corrected to Map<String, String>
   final String? description;
-  final DateTime startDate;
-  final DateTime endDate;
-  final DateTime startTime;
-  final DateTime endTime;
-  final RepetionRule repetitionRule;
+  final DateTime startDateTime;
+  final DateTime endDateTime;
+  final RepetionRule? repetitionRule;
   final bool oneDayEvent;
   final String? location;
   final DateTime createdAt;
   final DateTime updatedAt;
 
   ClassModel({
-    this.classId,
     required this.className,
     required this.teacherId,
     this.description,
     this.coTeacherId,
-    required this.startDate,
-    required this.endDate,
-    required this.startTime,
-    required this.endTime,
-    required this.repetitionRule,
+    required this.startDateTime,
+    required this.endDateTime,
+    this.repetitionRule,
     required this.oneDayEvent,
     this.location,
     required this.createdAt,
@@ -34,14 +28,11 @@ class ClassModel {
   });
 
   toJson() => {
-        "ClassId": classId,
         "ClassName": className,
         "TeacherId": teacherId,
         "CoTeacherId": coTeacherId, // Corrected to Map<String, String>
-        "StartDate": startDate,
-        "EndDate": endDate,
-        "StartTime": startTime,
-        "EndTime": endTime,
+        "StartDate": startDateTime,
+        "EndDate": endDateTime,
         "RepetitionRule": repetitionRule,
         "OneDayEvent": oneDayEvent,
         "Location": location,
