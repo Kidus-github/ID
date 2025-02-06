f02c90a79db674054c4afee8e81f90799b82208b
Kidus-github <kidus.muluwork@gmail.com>
2025-02-06 20:00:48 +0300

    Integrated Google sign In feature to it

--- lib/src/controllers/signin_controller.dart
+++ lib/src/controllers/signin_controller.dart
@@ -1,17 +1,67 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:id/src/constants/image_string.dart';
+import 'package:id/src/controllers/user_controller.dart';
 import 'package:id/src/repository/authentication_repository/authentication_repository.dart';
+import 'package:id/src/utils/loader/loaders.dart';
+import 'package:id/src/utils/network_manager/network_manager.dart';
+import 'package:id/src/utils/popups/full_screen_loader.dart';
 
 class SignInController extends GetxController {
   static SignInController get instance => Get.find();
+
+  // final localStorage = GetStorage();
   final email = TextEditingController();
   final password = TextEditingController();
   final formKey = GlobalKey<FormState>();
   final isPasswordHidden = true.obs;
+  final userController = Get.put(UserController());
 
-  void loginUser() {
-    AuthenticationRepository.instance
-        .loginWithEmailAndPassword(email.text.trim(), password.text.trim());
+  void loginUser() async {
+    try {
+      FullScreenLoader.openLoadingDialog(
+          'Logging you in....', ksuccessfullyRegisterAnimation);
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        FullScreenLoader.stopLoading();
+        return;
+      }
+      if (!formKey.currentState!.validate()) {
+        FullScreenLoader.stopLoading();
+      }
+
+      final UserCredential = await AuthenticationRepository.instance
+          .loginWithEmailAndPassword(email.text.trim(), password.text.trim());
+
+      FullScreenLoader.stopLoading();
+
+      AuthenticationRepository.instance.screenRedirect();
+    } catch (e) {
+      FullScreenLoader.stopLoading();
+      Loaders.errorSnackBar(title: 'Oh Snap', message: e.toString());
+    }
+  }
+
+  void googleSignIn() async {
+    try {
+      FullScreenLoader.openLoadingDialog(
+          'Logging you in....', ksuccessfullyRegisterAnimation);
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        FullScreenLoader.stopLoading();
+        return;
+      }
+      final userCredential =
+          await AuthenticationRepository.instance.signInWithGoogle();
+      await userController.saveUserRecord(userCredential);
+      FullScreenLoader.stopLoading();
+      AuthenticationRepository.instance.screenRedirect();
+    } catch (e) {
+      FullScreenLoader.stopLoading();
+      Loaders.errorSnackBar(title: 'Oh Snap', message: e.toString());
+    }
   }
 
   @override
---
+++ lib/src/controllers/user_controller.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:id/src/models/user_model.dart';
+import 'package:id/src/utils/loader/loaders.dart';
+
+import '../repository/user_repository/user_repository.dart';
+
+class UserController extends GetxController {
+  static UserController get instance => Get.find();
+  Rx<UserModel> user = UserModel.empty().obs;
+  final userRepository = Get.put(UserRepository());
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchUserRecord();
+  }
+
+  Future<void> fetchUserRecord() async {
+    try {
+      final user = await userRepository.fetchUserDetails();
+      this.user(user);
+    } catch (e) {
+      user(UserModel.empty());
+    }
+  }
+
+  Future<void> saveUserRecord(UserCredential? userCredential) async {
+    try {
+      if (userCredential != null) {
+        final user = UserModel(
+          id: userCredential.user!.uid,
+          firstName: userCredential.user!.displayName ?? '',
+          middleName: userCredential.user!.displayName!.split("").length > 1
+              ? userCredential.user!.displayName!.split("").sublist(1).join('')
+              : '',
+          email: userCredential.user!.email ?? '',
+          phoneNo: userCredential.user!.phoneNumber ?? '',
+          nfcTagId: '',
+          profilePic: userCredential.user!.photoURL,
+          createdAt: DateTime.now(),
+          updatedAt: DateTime.now(),
+        );
+
+        await UserRepository.instance.createUser(user);
+      }
+    } catch (e) {
+      Loaders.errorSnackBar(
+          title: 'Data not saved',
+          message:
+              'Something went wrong while saving your infomation. You can re-save your data in your Profile.');
+    }
+  }
+}
