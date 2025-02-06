fd756d42b1aa9b0762210fa59daceeb0ac54f292
Kidus-github <kidus.muluwork@gmail.com>
2025-02-06 20:01:41 +0300

    modified the code to work wit CRUD operation

--- lib/src/models/user_model.dart
+++ lib/src/models/user_model.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class UserModel {
   final String id;
@@ -11,16 +12,17 @@ class UserModel {
   final DateTime createdAt;
   final DateTime updatedAt;
 
-  UserModel(
-      {required this.createdAt,
-      required this.firstName,
-      required this.email,
-      required this.middleName,
-      required this.nfcTagId,
-      required this.phoneNo,
-      required this.updatedAt,
-      required this.id,
-      this.profilePic});
+  UserModel({
+    required this.id,
+    required this.firstName,
+    required this.middleName,
+    required this.email,
+    required this.phoneNo,
+    required this.nfcTagId,
+    this.profilePic,
+    required this.createdAt,
+    required this.updatedAt,
+  });
 
   toJson() => {
         "id": id,
@@ -33,21 +35,32 @@ class UserModel {
         "CreatedAt": createdAt,
         "ProfilePic": profilePic,
       };
+  static UserModel empty() => UserModel(
+      id: '',
+      firstName: '',
+      middleName: '',
+      email: '',
+      phoneNo: '',
+      nfcTagId: '',
+      profilePic: '',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now());
 
-  // factory UserModel.fromSnapshot(
-  //     DocumentSnapshot<Map<String, dynamic>> document) {
-  //   if (document.data() != null) {
-  //     final data = document.data()!;
-  //     return UserModel(
-  //         createdAt: data['createdAt'] ?? '',
-  //         firstName: data['firstName'] ?? ' ',
-  //         email: data['email'] ?? '',
-  //         middleName: data['middleName'] ?? '',
-  //         nfcTagId: data['nfcTagId'] ?? '',
-  //         phoneNo: data['phoneNo'] ?? '',
-  //         updatedAt: data['pdatedAt'] ?? '',
-  //         id: document.id);
-  //   }
-  // }
-  // );
+  factory UserModel.fromSnapshot(
+      DocumentSnapshot<Map<String, dynamic>> document) {
+    if (document.data() != null) {
+      final data = document.data()!;
+      return UserModel(
+          createdAt: data['createdAt'] ?? '',
+          firstName: data['firstName'] ?? ' ',
+          email: data['email'] ?? '',
+          middleName: data['middleName'] ?? '',
+          nfcTagId: data['nfcTagId'] ?? '',
+          phoneNo: data['phoneNo'] ?? '',
+          updatedAt: data['pdatedAt'] ?? '',
+          id: document.id);
+    } else {
+      return UserModel.empty();
+    }
+  }
 }
--- lib/src/repository/user_repository/user_repository.dart
+++ lib/src/repository/user_repository/user_repository.dart
@@ -1,37 +1,128 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:id/src/exceptions/firebase_auth_exception.dart';
+import 'package:id/src/exceptions/firebase_exceptions.dart';
+import 'package:id/src/exceptions/format_exception.dart';
+import 'package:id/src/exceptions/platform_exceptions.dart';
 import 'package:id/src/models/user_model.dart';
+import 'package:id/src/repository/authentication_repository/authentication_repository.dart';
 
 class UserRepository extends GetxController {
   static UserRepository get instance => Get.find();
 
   final _db = FirebaseFirestore.instance;
 
-  createUser(UserModel user) async {
-    print('I was here for database');
-    await _db
-        .collection("User")
-        .doc(user.id)
-        .set(user.toJson())
-        .whenComplete(
-          () => Get.snackbar(
-            "Success",
-            "You account has been created.",
-            snackPosition: SnackPosition.BOTTOM,
-            backgroundColor: Colors.green.withOpacity(0.1),
-            colorText: Colors.green,
-          ),
-        )
-        .catchError((error, StackTrace) {
-      Get.snackbar(
-        "Error",
-        "Something went wrong. Try again",
-        snackPosition: SnackPosition.BOTTOM,
-        backgroundColor: Colors.redAccent.withOpacity(0.1),
-        colorText: Colors.red,
-      );
-      print(error.toString());
-    });
+  Future<void> createUser(UserModel user) async {
+    try {
+      await _db.collection("User").doc(user.id).set(user.toJson());
+    } on FirebaseAuthExceptions catch (e) {
+      throw FirebaseAuthExceptions(e.code).message;
+    } on FirebaseExceptions catch (e) {
+      throw FirebaseExceptions(e.code).message;
+    } on FormatExceptions catch (_) {
+      throw const FormatExceptions();
+    } on PlatformExceptions catch (e) {
+      throw PlatformExceptions(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again';
+    }
   }
+
+  Future<UserModel> fetchUserDetails() async {
+    try {
+      final documentSnapshot = await _db
+          .collection("User")
+          .doc(AuthenticationRepository.instance.authUser?.uid)
+          .get();
+      if (documentSnapshot.exists) {
+        return UserModel.fromSnapshot(documentSnapshot);
+      } else {
+        return UserModel.empty();
+      }
+    } on FirebaseAuthExceptions catch (e) {
+      throw FirebaseAuthExceptions(e.code).message;
+    } on FirebaseExceptions catch (e) {
+      throw FirebaseExceptions(e.code).message;
+    } on FormatExceptions catch (_) {
+      throw const FormatExceptions();
+    } on PlatformExceptions catch (e) {
+      throw PlatformExceptions(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again';
+    }
+  }
+
+  Future<void> updateUserDetails(UserModel updateUser) async {
+    try {
+      await _db
+          .collection("User")
+          .doc(updateUser.id)
+          .update(updateUser.toJson());
+    } on FirebaseAuthExceptions catch (e) {
+      throw FirebaseAuthExceptions(e.code).message;
+    } on FirebaseExceptions catch (e) {
+      throw FirebaseExceptions(e.code).message;
+    } on FormatExceptions catch (_) {
+      throw const FormatExceptions();
+    } on PlatformExceptions catch (e) {
+      throw PlatformExceptions(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again';
+    }
+  }
+
+  Future<void> updateSingleField(Map<String, dynamic> json) async {
+    try {
+      await _db
+          .collection("User")
+          .doc(AuthenticationRepository.instance.authUser?.uid)
+          .update(json);
+    } on FirebaseAuthExceptions catch (e) {
+      throw FirebaseAuthExceptions(e.code).message;
+    } on FirebaseExceptions catch (e) {
+      throw FirebaseExceptions(e.code).message;
+    } on FormatExceptions catch (_) {
+      throw const FormatExceptions();
+    } on PlatformExceptions catch (e) {
+      throw PlatformExceptions(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again';
+    }
+  }
+
+  Future<void> removeUserRecord(String userId) async {
+    try {
+      await _db.collection("User").doc(userId).delete();
+    } on FirebaseAuthExceptions catch (e) {
+      throw FirebaseAuthExceptions(e.code).message;
+    } on FirebaseExceptions catch (e) {
+      throw FirebaseExceptions(e.code).message;
+    } on FormatExceptions catch (_) {
+      throw const FormatExceptions();
+    } on PlatformExceptions catch (e) {
+      throw PlatformExceptions(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again';
+    }
+  }
+  // .whenComplete(
+  //   () => Get.snackbar(
+  //     "Success",
+  //     "You account has been created.",
+  //     snackPosition: SnackPosition.BOTTOM,
+  //     backgroundColor: Colors.green.withOpacity(0.1),
+  //     colorText: Colors.green,
+  //   ),
+  // )
+  //   .catchError((error, StackTrace) {
+  // Get.snackbar(
+  //   "Error",
+  //   "Something went wrong. Try again",
+  //   snackPosition: SnackPosition.BOTTOM,
+  //   backgroundColor: Colors.redAccent.withOpacity(0.1),
+  //   colorText: Colors.red,
+  // );
+  //   print(error.toString());
+  // });
 }
