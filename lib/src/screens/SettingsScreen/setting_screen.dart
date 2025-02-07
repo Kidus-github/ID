92011d355e8f69e84e93df7a7e8151c2af436085
Kidus-github <kidus.muluwork@gmail.com>
2025-02-07 21:14:25 +0300

    created new screens

--- lib/src/screens/SettingsScreen/setting_screen.dart
+++ lib/src/screens/SettingsScreen/setting_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:id/src/common_widget/bottom_nav_bar.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Account'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: const Text('Setting'),
+      ),
+      bottomNavigationBar: const BottomNavBar(),
+    );
   }
 }
