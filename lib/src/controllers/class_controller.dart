55cdbfebc8bc81476971c85f10fb8b086f434780
Kidus-github <kidus.muluwork@gmail.com>
2025-02-14 22:40:19 +0300

    modified the class controller to store on the firebase

--- lib/src/controllers/class_controller.dart
+++ lib/src/controllers/class_controller.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:id/src/controllers/user_controller.dart';
 import 'package:id/src/models/class_model.dart';
 import 'package:id/src/repository/class_repository/class_repository.dart';
 
+import '../constants/image_string.dart';
+import '../repository/authentication_repository/authentication_repository.dart';
+import '../utils/loader/loaders.dart';
+import '../utils/network_manager/network_manager.dart';
+import '../utils/popups/full_screen_loader.dart';
+
 class ClassController extends GetxController {
   static ClassController get instance => Get.find();
-
+  final formKey = GlobalKey<FormState>();
   final className = TextEditingController();
-  // final dataSourse = [].obs;
   final coTeacherId =
       TextEditingController(); // Corrected check for a valid user
   final startDateTime = DateTime.now().obs;
@@ -16,14 +22,70 @@ class ClassController extends GetxController {
   final oneDayEvent = false.obs;
   final location = TextEditingController();
   final description = TextEditingController();
-  // final  createdAt;
-  // final  updatedAt;
-  // final  teacherId;
+  DateTime created = DateTime.now();
+
+  final AuthenticationRepository auth = Get.find();
 
   final classRepo = Get.put(ClassRepository());
 
-  Future<void> createClass(ClassModel classModel) async {
-    await ClassRepository.instance.createClass(classModel);
-    print("tried and done ");
+  Future<void> createClass() async {
+    try {
+      FullScreenLoader.openLoadingDialog(
+        "Creating The Class...",
+        kLoader,
+        showAction: false, // No action button, so no need for actionText
+      );
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        FullScreenLoader.stopLoading();
+        return;
+      }
+      if (!formKey.currentState!.validate()) {
+        FullScreenLoader.stopLoading();
+        // print('pass 1.1');
+        return;
+      }
+
+      // ✅ Get the logged-in user
+      final user = auth.authUser;
+      if (user == null) {
+        FullScreenLoader.stopLoading();
+        Loaders.errorSnackBar(
+            title: 'Authentication Error',
+            message: 'You need to be logged in to create a class.');
+        return;
+      }
+
+      final newClass = ClassModel(
+        className: className.text.trim(),
+        teacherId: user.uid,
+        coTeacherId: coTeacherId.text.isNotEmpty
+            ? coTeacherId.text.split(',').map((e) => e.trim()).toList()
+            : [],
+        description: description.text,
+
+        startDateTime: startDateTime.value,
+        endDateTime: endDateTime.value,
+        // repetitionRule:
+        //     RepetionRule.fromString(controller.repetitionRule.text),
+        oneDayEvent: oneDayEvent.value,
+        location: location.text.trim(),
+        createdAt: created,
+        updatedAt: created,
+      );
+
+      await ClassRepository.instance.createClass(newClass);
+      FullScreenLoader.stopLoading();
+
+      Loaders.successSnackBar(
+          title: 'Success', message: "Your class has been created.");
+
+      print("tried and done ");
+    } catch (e) {
+      Loaders.errorSnackBar(
+          title: 'Data not saved',
+          message:
+              'Something went wrong while saving your infomation. You can re-save your data in your Profile.');
+    }
   }
 }
--- lib/src/repository/class_repository/class_repository.dart
+++ lib/src/repository/class_repository/class_repository.dart
@@ -1,7 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:id/src/exceptions/firebase_auth_exception.dart';
 import 'package:id/src/models/class_model.dart';
+
+import '../../exceptions/firebase_exceptions.dart';
+import '../../exceptions/format_exception.dart';
+import '../../exceptions/platform_exceptions.dart';
 
 class ClassRepository extends GetxController {
   static ClassRepository get instance => Get.find();
@@ -10,27 +15,18 @@ class ClassRepository extends GetxController {
 
   createClass(ClassModel classModel) async {
     print("create Class method");
-    await _db
-        .collection("Class")
-        .add(classModel.toJson())
-        .whenComplete(
-          () => Get.snackbar(
-            "Success",
-            "Class hase been Created",
-            snackPosition: SnackPosition.BOTTOM,
-            backgroundColor: Colors.green.withOpacity(0.1),
-            colorText: Colors.green,
-          ),
-        )
-        .catchError((error, StackTrace) {
-      Get.snackbar(
-        "Error",
-        "Something went wrong. Try again",
-        snackPosition: SnackPosition.BOTTOM,
-        backgroundColor: Colors.redAccent.withOpacity(0.1),
-        colorText: Colors.red,
-      );
-      print(error.toString());
-    });
+    try {
+      await _db.collection("Class").add(classModel.toJson());
+    } on FirebaseAuthExceptions catch (e) {
+      throw FirebaseAuthExceptions(e.code).message;
+    } on FirebaseExceptions catch (e) {
+      throw FirebaseExceptions(e.code).message;
+    } on FormatExceptions catch (_) {
+      throw const FormatExceptions();
+    } on PlatformExceptions catch (e) {
+      throw PlatformExceptions(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again';
+    }
   }
 }
